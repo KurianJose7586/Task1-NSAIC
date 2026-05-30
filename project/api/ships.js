92cd983bd9037afe339dc842e25e909ec1752076
Kurian Jose <140304094+KurianJose7586@users.noreply.github.com>
2026-05-30 21:09:30 +0530

    Refactor ships API to read from file system

--- project/api/ships.js
+++ project/api/ships.js
@@ -1,5 +1,9 @@
-import ships from "../data/ships.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
 
-export default function (req, res) {
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "data", "ships.json");
+  const ships = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
   res.status(200).json(ships);
 }
