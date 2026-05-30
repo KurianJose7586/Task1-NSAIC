01cfe24ee1d1116bc0fa65c2bf3702255e4078db
Kurian Jose <140304094+KurianJose7586@users.noreply.github.com>
2026-05-30 21:20:14 +0530

    Update API handler to return ship data from file

--- project/api/ships.js
+++ project/api/ships.js
@@ -1,5 +1,9 @@
+import fs from "fs";
+
 export default function handler(req, res) {
-  res.status(200).json({
-    message: "API works"
-  });
+  const ships = JSON.parse(
+    fs.readFileSync(new URL("./data.json", import.meta.url), "utf8")
+  );
+
+  res.status(200).json(ships);
 }
